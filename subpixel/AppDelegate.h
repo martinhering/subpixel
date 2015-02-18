6c913fe3c1db3330d585f5e2d7e192efab36bd3c
Martin Hering <hering@vemedio.com>
2015-02-18 10:15:12 +0100

    Initial Commit

---
+++ subpixel/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  subpixel
+//
+//  Created by Martin Hering on 18.02.15.
+//  Copyright (c) 2015 Vemedio. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ subpixel/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  subpixel
+//
+//  Created by Martin Hering on 18.02.15.
+//  Copyright (c) 2015 Vemedio. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
